f42a310c1928730c1deb8ac8e9232c9fb95e9960
gamesukimanIRS <gamesukiman@gmail.com>
2018-01-13 19:34:00 +0900

    Update PmChair.php

--- src/maru/PmChair.php
+++ src/maru/PmChair.php
@@ -62,7 +62,7 @@ class PmChair extends PluginBase implements Listener {
 					$addEntityPacket->type = Item::NETWORK_ID;
 					
 					$setEntityLinkPacket = new SetEntityLinkPacket();
-					$setEntityLinkPacket->link = [$addEntityPacket->entityRuntimeId, $player->getId(), ord("true")];
+					$setEntityLinkPacket->link = [$addEntityPacket->entityRuntimeId, $player->getId(), ord("true"), 0];
 					
 					foreach ( $this->getServer ()->getOnlinePlayers () as $target ) {
 						$target->dataPacket ( $addEntityPacket );
@@ -71,7 +71,7 @@ class PmChair extends PluginBase implements Listener {
 						}
 					}
 					
-					$setEntityLinkPacket->link = [$addEntityPacket->entityRuntimeId, 0, ord("true")];
+					$setEntityLinkPacket->link = [$addEntityPacket->entityRuntimeId, 0, ord("true"), 0];
 					$player->dataPacket ( $setEntityLinkPacket );
 					unset($this->doubleTap[$player->getName()]);
 				} else {
